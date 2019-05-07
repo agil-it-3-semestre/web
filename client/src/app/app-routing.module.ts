2a490cc389cd9dc4127a8d252d7bb2c68a2bf9e8
Lucas <goncalves.lucasmatheus@gmail.com>
2019-05-07 20:18:31 -0300

    Criação dos components Login & Home

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Authentication } from './services/auth/authentication';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // login
+  {
+    path: '',
+    component: LoginComponent
+  },
+  // home
+  {
+      path: 'home',
+      component: HomeComponent,
+      canActivate: [ Authentication    ]
+  },
+  // login
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { Authentication } from './services/auth/authentication';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    Authentication
+  ],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
